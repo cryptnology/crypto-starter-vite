f649fc7c298bc4ac2c719812378b919f2a2c0e81
cryptnology <jamie@cryptnology.dev>
2023-07-29 08:48:25 +0100

    add thirdweb sdk packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { ConnectWallet } from '@thirdweb-dev/react';
+
+const App = () => {
+  return (
+    <ConnectWallet
+      theme="dark"
+      btnTitle="Connect Wallet"
+      dropdownPosition={{
+        side: 'bottom', // "top" | "bottom" | "left" | "right";
+        align: 'end', // "start" | "center" | "end";
+      }}
+    />
+  );
+};
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThirdwebProvider, metamaskWallet } from '@thirdweb-dev/react';
+import { Localhost, Sepolia } from '@thirdweb-dev/chains';
 
 import App from './App.tsx';
 
@@ -9,7 +11,14 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThirdwebProvider
+        activeChain={Localhost}
+        supportedChains={[Localhost, Sepolia]}
+        supportedWallets={[metamaskWallet()]}
+        clientId={import.meta.env.VITE_THIRDWEB_CLIENT_ID as string}
+      >
+        <App />
+      </ThirdwebProvider>
     </Router>
   </React.StrictMode>,
 );
