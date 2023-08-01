276a4b51c85927f936b1f5ae0add5dc1c0945d75
cryptnology <jamie@cryptnology.dev>
2023-08-01 14:14:22 +0100

    Fix fund card css

--- frontend/src/components/FundCard.tsx
+++ frontend/src/components/FundCard.tsx
@@ -1,9 +1,9 @@
 import { ethers } from 'ethers';
-import { tagType } from '../assets';
 import { daysLeft } from '../utils';
 
 interface FundCardProps {
   owner: string;
+  name: string;
   title: string;
   description: string;
   target: string;
@@ -15,6 +15,7 @@ interface FundCardProps {
 
 const FundCard = ({
   owner,
+  name,
   title,
   description,
   target,
@@ -27,61 +28,54 @@ const FundCard = ({
 
   return (
     <div
-      className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer"
+      className="sm:w-[288px] w-full rounded-[15px] bg-light border-2 border-dark dark:bg-primaryDark dark:border-0 cursor-pointer"
       onClick={handleClick}
     >
       <img
         src={image}
         alt="fund"
-        className="w-full h-[158px] object-cover rounded-[15px]"
+        className="w-full h-[158px] p-0.5 dark:p-0 rounded-[12px] dark:rounded-[15px]"
       />
-
       <div className="flex flex-col p-4">
-        <div className="flex flex-row items-center mb-[18px]">
-          <img
-            src={tagType}
-            alt="tag"
-            className="w-[17px] h-[17px] object-contain"
-          />
-          <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">
-            Education
-          </p>
-        </div>
-
         <div className="block">
-          <h3 className="font-epilogue font-semibold text-[16px] text-white text-left leading-[26px] truncate">
+          <h3 className="font-epilogue font-semibold text-[16px] text-dark dark:text-light text-left leading-[26px] truncate">
             {title}
           </h3>
-          <p className="mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate">
+          <p className="mt-[5px] font-epilogue font-normal text-dark dark:text-[#808191] text-left leading-[18px] truncate">
             {description}
           </p>
         </div>
 
         <div className="flex justify-between flex-wrap mt-[15px] gap-2">
           <div className="flex flex-col">
-            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+            <h4 className="font-epilogue font-semibold text-[14px] text-dark/60 dark:text-[#b2b3bd] leading-[22px]">
               {ethers.utils.formatUnits(amountCollected, 'ether')}
             </h4>
-            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-dark dark:text-[#808191] sm:max-w-[120px] truncate">
               Raised of {ethers.utils.formatUnits(target, 'ether')}
             </p>
           </div>
           <div className="flex flex-col">
-            <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">
+            <h4 className="font-epilogue font-semibold text-[14px] text-dark/60 dark:text-[#b2b3bd] leading-[22px]">
               {remainingDays}
             </h4>
-            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-dark dark:text-[#808191] sm:max-w-[120px] truncate">
               {remainingDays === '1' ? 'Day Left' : 'Days Left'}
             </p>
           </div>
         </div>
-
-        <div className="flex items-center mt-[20px] gap-[12px]">
-          <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#13131a]">
-            {/* <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain"/> */}
+        <div className="mt-[20px] gap-[12px]">
+          <div className="flex-1 font-epilogue font-bold text-[12px] text-[#808191] truncate">
+            Campaigner:{' '}
+            <span className="text-dark/60 font-normal dark:text-[#b2b3bd]">
+              {name}
+            </span>
           </div>
-          <p className="flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate">
-            by <span className="text-[#b2b3bd]">{owner}</span>
+          <p className="flex-1 font-epilogue font-bold text-[12px] text-[#808191] truncate">
+            Address:{' '}
+            <span className="text-dark/60 font-normal dark:text-[#b2b3bd]">
+              {owner}
+            </span>
           </p>
         </div>
       </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,18 @@
 import { useEffect, useState } from 'react';
-import { Contract } from 'ethers';
 
-import { loadCampaigns, useCryptoStarterStore, useUserStore } from '../store';
+import { useCryptoStarterStore, useUserStore } from '../store';
 import { DisplayCampaigns } from '../components';
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [campaigns, setCampaigns] = useState([]);
 
-  const { contract, setCampaigns: contractSetCampaigns } =
-    useCryptoStarterStore();
+  const { campaigns, contract } = useCryptoStarterStore();
   const { account } = useUserStore();
 
-  const fetchCampaigns = async () => {
-    setIsLoading(true);
-    const data = await loadCampaigns(
-      contract as Contract,
-      contractSetCampaigns,
-    );
-    setCampaigns(data);
-    setIsLoading(false);
-  };
-
   useEffect(() => {
-    if (contract) fetchCampaigns();
+    if (!contract && !campaigns) return;
+    setIsLoading(true);
+    if (campaigns) setIsLoading(false);
   }, [account, contract]);
 
   return (
