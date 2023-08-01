3599ffa69b293af727631e80bed9fba09fee49a8
cryptnology <jamie@cryptnology.dev>
2023-08-01 13:03:33 +0100

    Display campaigns

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Container } from './Container';
 export { default as CustomButton } from './CustomButton';
+export { default as DisplayCampaigns } from './DisplayCampaigns';
 export { default as FormField } from './FormField';
 export { default as Loader } from './Loader';
 export { default as NavBar } from './NavBar';
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,38 @@
-import { useCryptoStarterStore } from "../store";
+import { useEffect, useState } from 'react';
+import { Contract } from 'ethers';
+
+import { loadCampaigns, useCryptoStarterStore, useUserStore } from '../store';
+import { DisplayCampaigns } from '../components';
 
 const Home = () => {
-  const {campaigns} = useCryptoStarterStore();
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
 
-  console.log(campaigns);
+  const { contract, setCampaigns: contractSetCampaigns } =
+    useCryptoStarterStore();
+  const { account } = useUserStore();
 
-  return <div>Home</div>;
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await loadCampaigns(
+      contract as Contract,
+      contractSetCampaigns,
+    );
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [account, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
 };
 
 export default Home;
