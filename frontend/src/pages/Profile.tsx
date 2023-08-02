9247b03b6ec88988593354480e28e5b2ee4085ec
cryptnology <jamie@cryptnology.dev>
2023-08-02 12:56:59 +0100

    Create profile page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,5 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import { DisplayCampaigns } from '../components';
+import {
+  loadUserCampaigns,
+  useCryptoStarterStore,
+  useUserStore,
+} from '../store';
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [userCampaigns, setUserCampaigns] = useState([]);
+
+  const { campaigns, contract } = useCryptoStarterStore();
+  const { account } = useUserStore();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = loadUserCampaigns(campaigns, account);
+    setUserCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [account, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={userCampaigns}
+    />
+  );
 };
 
 export default Profile;
